165a3ed0a336463f03b8d63014bc2588d4f7985c
Matthew S. Baran <matthewsbaran@gmail.com>
2023-08-19 22:30:07 -0400

    Add python environment file and format .py files

--- python/edit_tex.py
+++ python/edit_tex.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
-import scipy
-import os
 
-img = plt.imread("gl-test/ocean_foam_texture_3_by_compasslogicstock_dar9asv-fullview.jpg")
+img = plt.imread(
+    "gl-test/ocean_foam_texture_3_by_compasslogicstock_dar9asv-fullview.jpg"
+)
 
 mask = np.sum(np.greater(img, 140).astype(float), axis=2)
-mask = np.logical_and(np.equal(mask, 3), np.greater(img[:,:,2], img[:,:,0])).astype(np.uint8) * 255
+mask = (
+    np.logical_and(np.equal(mask, 3), np.greater(img[:, :, 2], img[:, :, 0])).astype(
+        np.uint8
+    )
+    * 255
+)
 
-out = np.stack((img[:,:,0], img[:,:,1], img[:,:,2], mask), axis=2)
+out = np.stack((img[:, :, 0], img[:, :, 1], img[:, :, 2], mask), axis=2)
 
 plt.imsave("foam.png", out)
--- python/gen_bathy.py
+++ python/gen_bathy.py
@@ -1,15 +1,11 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 import scipy
-import os
-
-from skimage.transform import resize
 
 vec1 = np.linspace(-3, 3, 256)
-vec2 = np.ones(shape=(256,1))
+vec2 = np.ones(shape=(256, 1))
 gradient = vec1 * vec2
-gradient = np.stack((gradient, gradient, gradient, np.zeros(shape=(256,256))), axis=2)
+gradient = np.stack((gradient, gradient, gradient, np.zeros(shape=(256, 256))), axis=2)
 
 img = np.random.normal(0.0, 1.0, size=(256, 256))
 img = scipy.ndimage.gaussian_filter(img, 10)
@@ -19,19 +15,19 @@ plt.imshow(img)
 img = (img - np.min(img)) / (np.max(img) - np.min(img))
 img = (img - 0.5) * 0.5
 
-img = np.stack((img, img, img, np.zeros(shape=(256,256))), axis=2)
+img = np.stack((img, img, img, np.zeros(shape=(256, 256))), axis=2)
 
 img = img + gradient
 
-[dx, dy] = np.gradient(img[:,:,1])
+[dx, dy] = np.gradient(img[:, :, 1])
 norms = np.stack((-dx, -dy, np.ones_like(dx)), axis=2)
 mag = np.sum(np.square(norms), axis=2)
-mag = np.stack((mag,mag,mag), axis=2)
+mag = np.stack((mag, mag, mag), axis=2)
 norms = np.divide(norms, mag)
 norms = (norms + 1) / 2
 plt.imsave("bathy_norms2.png", norms)
 
-img = (img - np.min(img)) / (np.max(img)-np.min(img))
+img = (img - np.min(img)) / (np.max(img) - np.min(img))
 plt.imsave("bathy2.png", img)
 
 plt.figure()
